65c21b583532ee636452d829eab5ec840236d17e
Jimachine <jimmy_onion@hotmail.com>
2019-04-26 12:23:54 +0100

    initial commit

---
+++ Isograms/Kata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isograms
+{
+    public class Kata
+    {
+        public static bool IsIsogram(string str)
+        {
+            HashSet<char> letters = new HashSet<char>();
+            foreach (char c in str)
+            {
+                letters.Add(c);
+            }
+            if (letters.Count < str.Length)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
